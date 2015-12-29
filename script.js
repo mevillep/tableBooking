39a507da685034a0d5b6cbe1e76cadc03ac68d8d
Mevillep <mevillepereira10@gmail.com>
2015-12-29 16:23:24 -0600

    docs(module-ui): update readme with features and functionality feat(module-ui): add bootstrap and fix javascript

--- script.js
+++ script.js
@@ -5,15 +5,57 @@
 
 
 
-$( document ).ready(function() {
 
-    $( ".generate" ).click(function( event ) {
 
-        alert( "As you can see, the link no longer took you to jquery.com" );
-        console.log(done);
+    $( document ).ready(function() {
+
+        $( ".ownerForm" ).hide();
+        $( ".customerForm" ).hide();
+        $( "#customerToggle" ).click(function() {
+
+            $( ".customerForm" ).toggle( "slow" );
+            $( ".ownerForm" ).hide();
+        });
+
+        $( "#ownerToggle" ).click(function() {
+            $( ".ownerForm" ).toggle();
+            $( ".customerForm" ).hide();
+        });
+
+
+
+//        var dateToday = new Date();
+//       // console.log(dateToday);
+//        $(function() {
+//            $( "#date" ).datepicker({
+//                numberOfMonths: 3,
+//                showButtonPanel: true,
+//                minDate: 0
+//            });
+//        });
+
+        $("#date").datepicker({ minDate: 0 });
+
+
+
+
+        $( ".generate" ).click(function() {
+
+            $(".customerForm").validate();
+//                $("#op").data("Your unique code is "+Math.floor(Math.random() * (1000)));
+
+        });
+
+        //setTimeout(function(){
+        //    $( "#detail" ).html( "<small>Enter your booking details</small>" );
+        //},5000)
+
+        $( "#edit" ).bind( "click", function() {
+            $( this ).alert( "Click to edit your reservation " );
+        });
 
 
 
     });
 
-});
+

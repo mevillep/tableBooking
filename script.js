78c58c479b6b64c120ea6d6ab48992787c3049a1
Mevillep <mevillepereira10@gmail.com>
2015-12-28 19:27:14 -0600

    feat(init): add(index, createReservationOwner, editReservation, owner, ownerRequirements, reservationDetails,seating, setting,style.css, script.js)

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Meville on 12/22/15.
+ */
+
+
+
+
+$( document ).ready(function() {
+
+    $( ".generate" ).click(function( event ) {
+
+        alert( "As you can see, the link no longer took you to jquery.com" );
+        console.log(done);
+
+
+
+    });
+
+});
